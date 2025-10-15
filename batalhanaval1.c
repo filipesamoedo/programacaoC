33666bcb5653cab2d81fdbfe380aaafdac5922bc
Filipe S. Amoedo <filipesamoedo@gmail.com>
2025-10-15 16:34:41 -0300

    Implement Battleship game board and ship placement
    Initial implementation of a Battleship game with a 10x10 board, including ship placement and display functionality.

---
+++ batalhanaval1.c
@@ -0,0 +1,70 @@
+// Desafio BATALHA NAVAL. Versão novato. Criar tabuleiro e exibir. Cadastrar dois navios de 3 posições e exibir.
+#include <stdio.h>
+
+int main ()
+{   
+    char linha[10] = {'A','B','C','D','E','F','G','H','I','J'};
+    int coluna[10] = {1,2,3,4,5,6,7,8,9,10};
+    
+    // Declaração do tabuleiro 10x10
+
+    int tabuleiro[10][10];
+    int i, j;
+
+    // Inicializa todas as posições com 0
+
+    for(i = 0; i < 10; i++) {
+        for(j = 0; j < 10; j++) {
+            tabuleiro[i][j] = 0; 
+    }
+    }
+
+    // FUNÇÃO IMPRIMIR TABULEIRO
+
+    int imprimir_tabuleiro() {
+
+        printf("TABULEIRO BATALHA NAVAL\n");
+
+        // Impressao dos números-base da coluna. 
+
+        printf(" "); // Espaço antes dos números da coluna
+        for (j = 0; j < 10; j++)
+        {
+        printf(" %d", coluna[j]);
+        }
+        printf("\n");
+
+        // Impresssão das letras-base da linha e do tabuleiro
+
+        for(i = 0; i < 10; i++) {
+            printf("%c", linha[i]); // Imprime ABC.. Antes de cada linha do tabuleiro
+            for(j = 0; j < 10; j++) {
+            printf(" %d", tabuleiro[i][j]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+
+    // Imprime o tabuleiro inicial
+    imprimir_tabuleiro();
+
+    // Escolher posição para navio1 de 3 lugares. Horizontal. F1 F2 F3
+
+    for(i = 0; i < 3; i++) {
+        tabuleiro[5][i] = 3; 
+    }
+
+    // Escolhendo posição para navio2 de 3 lugares. Vertical. D8 E8 F8
+    
+    for(i = 3; i < 6; i++) {
+        tabuleiro[i][7] = 3; // Coloca o navio na coluna 7, nas linhas 3,4 e 5
+    }
+
+    // Imprime o tabuleiro atualizado
+
+    printf("\n 2 navios cadastrados: \n");
+    imprimir_tabuleiro();
+
+    return 0;
+}
